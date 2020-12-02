238079af5d9f46e7ffcd9e45af33f73c3174d16c
rahulpathak1103 <64638364+rahulpathak1103@users.noreply.github.com>
2020-12-02 12:56:08 +0530

    updatedtest

--- sending_mail.py
+++ sending_mail.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
-from email import encoders
+from email import encoders 
 
 
 class MailSend:
